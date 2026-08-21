15c0db7105691d606cbbe96946ec7a3b442498cf
dedé <dehmicali5@gmail.com>
2026-08-21 20:58:51 -0300

    Iniciando Projeto

---
+++ Sakkola/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sakkola.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
